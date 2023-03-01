56c7776e3fa96b107b1f7c9db8a2e3e6970a5af9
Dejan <dejanmirceticm@gmail.com>
2023-03-01 22:07:33 +0100

    cleaning up.

--- R/nnet.R
+++ R/nnet.R
@@ -10,7 +10,7 @@ nn
 
 summary(nn)
 
-#summary(nn$residuals)
+summary(nn$residuals)
 
 nn.pred=predict(nn,test2, type="class")
 
@@ -25,7 +25,7 @@ test.err=double(10)
 AUC.err=double(10)
 f.err=double(10)
 for(size in 1:10){
-  fit=nnet(marks~.,data=train2, size=size, maxit=500, decay=0.001,rang = 0.1)
+  fit=nnet(marks~.,data=train2, size=size, maxit=500, decay=0.001)
   nn.pred=predict(fit,test.data, type="class")
   test.err[size]=(classificationMetrics(nn.pred,test.data[,"marks"])["err"]*100)
   AUC.err[size]=roc.curve(as.factor(test.data[,"marks"]),as.factor(nn.pred),FALSE)$auc
@@ -43,10 +43,10 @@ matplot(1:size,f.err,pch=19,col=c("red"),type="b",ylab="F Error")
 legend("topright",legend=c("F"),pch=19,col=c("red"))#From the graph mtry=10
 
 #Odluka je da se ide na set up 10 neurona
-source("cutoff_nn.R")
+source("cutoff.R")
 
-nn.probs=predict(fit,test2)
-cut_nn <- cutoff(as.numeric(nn.probs))
+nn.probs=predict(nn,test2)
+cut_nn <- cutoff_nn(nn.probs)
 
 
 i<-cut_nn
@@ -54,17 +54,15 @@ nn.pred=ifelse(nn.probs>i,"TA","Direct")
 t<-table(nn.pred,ClassTable[-train,"marks"]);print(t)
 (t[1,2]/(t[1,2]+t[2,2]))#FN
 (t[2,1]/(t[1,1]+t[2,1]))#FP
-1-mean(nn.pred==ClassTable[-train,"marks"])
+1-mean(lr.pred==ClassTable[-train,"marks"])
 
 #Znaci error ce biti 0.43
 
 #AUC 0.55
 roc.curve(as.factor(test.data[,"marks"]),as.factor(nn.pred),FALSE)$auc
 
-# F 0.48 ali ti to nasminkaj sa macroF
 
 (classificationMetrics(nn.pred,test.data[,"marks"])["F"])
-#Na lap topu ispada dobar broj oko 0.52
 (classificationMetrics(nn.pred,test.data[,"marks"])["macroF"])
 #Promjeni ono gore da ti ne daje klasu nego vjerovatnocu.
 
@@ -72,66 +70,3 @@ roc.curve(as.factor(test.data[,"marks"]),as.factor(nn.pred),FALSE)$auc
 
 #Classification example with Neuralnet in R
 
-#https://medium.com/@yolandawiyono98/ann-classification-with-nnet-package-in-r-3c4dc14d1f14
-#https://amunategui.github.io/multinomial-neuralnetworks-walkthrough/index.html
-#https://parsnip.tidymodels.org/reference/details_mlp_nnet.html
-#https://parsnip.tidymodels.org/index.html
-
-library(neuralnet)
-
-# https://www.r-bloggers.com/2017/06/neural-networks-exercises-part-2/
-# https://journal.r-project.org/archive/2010-1/RJournal_2010-1_Guenther+Fritsch.pdf
-# https://www.r-bloggers.com/2013/07/using-neural-networks-for-credit-scoring-a-simple-example/
-
-# R-Session 11 - Statistical Learning - Neural Networks
-#https://www.youtube.com/watch?v=lTMqXSSjCvk
-
-n2=neuralnet(marks~.-origin,data=train2, hidden = c(4,3), linear.output = FALSE)
-plot(n2)
-
-#Probaj da uporedis ova dva stavi isto podesavanje.
-
-x <- 10000
-
-n1=neuralnet(marks~.-origin,data=train2[1:x,], hidden = c(10), linear.output = FALSE)
-plot(n1)
-
-n1$net.result[[1]]
-
-n1.probs <- predict(n1,test2[1:x,])[,1]
-
-#cut_nn <- cutoff_nn(n1.probs)
-
-#i<-cut_nn
-n1.pred=ifelse(n1.probs>0.5,"TA","Direct")
-t<-table(as.factor(n1.pred),test2[1:x,"marks"]);print(t)
-(t[1,2]/(t[1,2]+t[2,2]))#FN
-(t[2,1]/(t[1,1]+t[2,1]))#FP
-1-mean(as.factor(n1.pred)==test2[1:x,"marks"])
-
-#Znaci error ce biti 0.43
-
-roc.curve(as.factor(test2[1:x,"marks"]),as.factor(n1.pred),FALSE)$auc
-
-
-### Backpropagation
-
-n2=neuralnet(marks~.-origin,data=train2[1:x,], hidden = c(4,3), linear.output = FALSE, algorithm = "backprop",learningrate=0.1)
-plot(n2)
-
-n2$net.result[[1]]
-
-n2.probs <- predict(n2,test2[1:x,])[,1]
-
-#cut_nn <- cutoff_nn(n1.probs)
-
-#i<-cut_nn
-n2.pred=ifelse(n2.probs>0.5,"TA","Direct")
-t<-table(as.factor(n2.pred),test2[1:x,"marks"]);print(t)
-(t[1,2]/(t[1,2]+t[2,2]))#FN
-(t[2,1]/(t[1,1]+t[2,1]))#FP
-1-mean(as.factor(n2.pred)==test2[1:x,"marks"])
-
-#Znaci error ce biti 0.43
-
-roc.curve(as.factor(test2[1:x,"marks"]),as.factor(n1.pred),FALSE)$auc
