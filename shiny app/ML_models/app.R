29da1fc8050c68ff4ea172844e09d7d8e74ed146
Dejan <dejanmirceticm@gmail.com>
2023-03-02 19:20:22 +0100

    shiny

--- shiny app/ML_models/app.R
+++ shiny app/ML_models/app.R
@@ -11,8 +11,9 @@ FT <- FT[,-26];FT <- FT[,c(1:2,11,20,25)];
 FT <- FT[-1,]#Izbacen Null model
 
 Tabela<-rbind(rep(round(max(FT[-1,])),dim(FT)[2]),rep(0,dim(FT)[2]),FT)
-rownames(Tabela) <- c("max","min","LR","LDA","QDA","KNN","Lasso","GAM","RF","Boosting","SVM")
+rownames(Tabela) <- c("max","min","LR","LDA","QDA","KNN","Lasso","GAM","RF","Boosting","SVM", "FNN", "DL_Torch","XG Boost","TensorFlow","RNN","CNN","DTW")
 colnames(Tabela) <- c("False positive", "False negative", "F-statistics","Misclassification error","AUC")
+
 
 #Ova druga tabela je dodata da bi prva kolona bili nazivi modela
 Tabela2 <- cbind(rownames(Tabela),Tabela)
@@ -29,7 +30,7 @@ ui <- fluidPage(
         sidebarPanel(width = 2,
                      #checkboxGroupInput(inputId="modeli", label="Select the machine learning models that you want to compare:", choices=list("Null"=3,"LR"=4,"LDA"=5,"QDA"=6,"KNN"=7,"Lasso"=8,"GAM"=9,"RF"=10,"Boosting"=11,"SVM"=12)), #ovo je sa Null Modelom
                      
-                     checkboxGroupInput(inputId="modeli", label="Select models:", choices=list("LR"=3,"LDA"=4,"QDA"=5,"KNN"=6,"Lasso"=7,"GAM"=8,"RF"=9,"Boosting"=10,"SVM"=11))
+                     checkboxGroupInput(inputId="modeli", label="Select models:", choices=list("LR"=3,"LDA"=4,"QDA"=5,"KNN"=6,"Lasso"=7,"GAM"=8,"RF"=9,"Boosting"=10,"SVM"=11,"FNN"=12, "DL_Torch"=13,"XG Boost"=14,"TensorFlow"=15,"RNN"=16,"CNN"=17,"DTW"=18))
         ),
         mainPanel(
             tabsetPanel(
@@ -51,7 +52,7 @@ server <- function(input, output) {
         
         if(length(input$modeli)!=0){
             radarchart(as.data.frame(Tabela)[c(1:2,as.numeric((input$modeli))),],axistype=2,cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,20,5), cglwd=0.8,vlcex=1.5,pty = 16,calcex=1.8,plwd = 2,seg=4, pcol=c(1,"slateblue2",3,4,5,6,"red",7,"orange2"), plty = 1)
-            legend("topright",legend=c(rownames(Tabela)[c(as.numeric((input$modeli)))]),pch=19,col=c(1,"slateblue2",3,4,5,6,"red",7,"orange2")[c(1:length(input$modeli))])
+            legend("topright",legend=c(rownames(Tabela)[c(as.numeric((input$modeli)))]),pch=19,col=c(1,"slateblue2",3,4,5,6,"red",7,"orange2",1,"slateblue2",3,4,5,6,"red")[c(1:length(input$modeli))])
         }
     })
     
